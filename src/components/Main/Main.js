cb11379b488105eda5cd15a4e9b07bb7fe2cf6ca
Kirill Mecker <ghost3299@yandex.ru>
2021-02-22 12:19:16 +0400

    feat: destarction menu

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import { Button, Form, ToggleButton, ButtonGroup } from 'react-bootstrap';
 import { AppContext } from '../../AppProvider';
 import { GAME_TYPES, PLAYER_TURNS } from '../../common';
 import Board from '../Board/Board';
+import Menu from '../Menu/Menu';
 import Music from '../Music/Music';
 import ToggleButtonExample from '../ToggleButtonExample/ToggleButtonExample';
 import './Main.css';
@@ -55,51 +56,51 @@ import './Main.css';
 //   );
 // }
 
-class Menu extends Component {
-  render() {
-    const computer = +localStorage.getItem('Computer');
-    const playerVSComputer = +localStorage.getItem('PlayerVSComputer');
-    const playerOne = +localStorage.getItem('Player1');
-    const playerTwo = +localStorage.getItem('Player2');
-    const tie = +localStorage.getItem('Tie');
-    return (
-      <div className='game-info'>
-        <div className='statistics'>
-          <h2>Statistics</h2>
-          <div>
-            <p className='statistics__tie'>Tie : {tie}</p>
-            <h4 className='statistics__subtitle'>Wins</h4>
-            <div className='statistics__wrapper'>
-              <div className='statistics__type'>
-                <h5>(2 player)</h5>
-                <p>Player 1 : {playerOne}</p>
-                <p>Player 2 : {playerTwo}</p>
-              </div>
-              <div className='statistics__type'>
-                <h5>(play with computer)</h5>
-                <p>Computer : {computer}</p>
-                <p>Player : {playerVSComputer}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='game-menu'>
-          <h2>Game menu</h2>
-          <ToggleButtonExample />
-          <Button
-            variant='warning'
-            className='btn_new-menu'
-            onClick={() => this.context.newGame()}
-          >
-            New Game
-          </Button>{' '}
-        </div>
-      </div>
-    );
-  }
-}
+// class Menu extends Component {
+//   render() {
+//     const computer = +localStorage.getItem('Computer');
+//     const playerVSComputer = +localStorage.getItem('PlayerVSComputer');
+//     const playerOne = +localStorage.getItem('Player1');
+//     const playerTwo = +localStorage.getItem('Player2');
+//     const tie = +localStorage.getItem('Tie');
+//     return (
+//       <div className='game-info'>
+//         <div className='statistics'>
+//           <h2>Statistics</h2>
+//           <div>
+//             <p className='statistics__tie'>Tie : {tie}</p>
+//             <h4 className='statistics__subtitle'>Wins</h4>
+//             <div className='statistics__wrapper'>
+//               <div className='statistics__type'>
+//                 <h5>(2 player)</h5>
+//                 <p>Player 1 : {playerOne}</p>
+//                 <p>Player 2 : {playerTwo}</p>
+//               </div>
+//               <div className='statistics__type'>
+//                 <h5>(play with computer)</h5>
+//                 <p>Computer : {computer}</p>
+//                 <p>Player : {playerVSComputer}</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className='game-menu'>
+//           <h2>Game menu</h2>
+//           <ToggleButtonExample />
+//           <Button
+//             variant='warning'
+//             className='btn_new-menu'
+//             onClick={() => this.context.newGame()}
+//           >
+//             New Game
+//           </Button>{' '}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-Menu.contextType = AppContext;
+// Menu.contextType = AppContext;
 
 // const Cell = (props) => {
 //   return (
